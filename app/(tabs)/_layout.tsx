12b53d260f64a92d78d3780818ccba3ada1a344a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-08-12 09:19:21 +0100

    fixing a navigation issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import { FontAwesome } from '@expo/vector-icons'
+import { COLORS } from '@/constants/theme' // Assuming you have this for colors
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name']
@@ -12,51 +13,44 @@ function TabBarIcon(props: {
 export default function TabLayout() {
   return (
     <Tabs
-      screenOptions={
-        {
-          // You can add global tab options here, e.g., active tint color
-        }
-      }
+      screenOptions={{
+        tabBarActiveTintColor: COLORS.primary, // Make active tabs blue
+        tabBarInactiveTintColor: COLORS.gray, // Make inactive tabs gray
+      }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="scanner"
-        options={{
-          title: 'Scanner',
-          tabBarIcon: ({ color }) => <TabBarIcon name="qrcode" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="add"
-        options={{
-          title: 'Add',
-          headerShown: false, // We use ScreenWrapper inside
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="plus-square" color={color} />
-          ),
         }}
       />
       <Tabs.Screen
         name="map"
         options={{
           title: 'Map',
-          headerShown: false, // We created a custom header overlay
+          headerShown: false,
           tabBarIcon: ({ color }) => (
             <TabBarIcon name="map-marker" color={color} />
           ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
+      {/* Our new "More" tab */}
+      <Tabs.Screen
+        name="more"
+        options={{
+          title: 'More',
+          headerShown: false, // We handle the header inside the screen
+          tabBarIcon: ({ color }) => <TabBarIcon name="bars" color={color} />,
         }}
       />
     </Tabs>
